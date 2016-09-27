82213f6f0ea08e04d96d6ae901cf1e677d2e15c1
Songho Lee <sl222xk@student.lnu.se>
2016-09-27 10:16:06 +0200

    Observer deleted from a member

--- Workshop2/src/model/Member.java
+++ Workshop2/src/model/Member.java
@@ -15,7 +15,6 @@ public class Member {
 	private String personalnumber;
 	private int memberID;
 	private ArrayList<Boat> boatdata = new ArrayList<Boat>();
-	private ArrayList<IMemberUpdateObserver> subscribers = new ArrayList<IMemberUpdateObserver>();
 
 	public Member() {
 
@@ -69,53 +68,31 @@ public class Member {
 	public void setMemberID(int memberID) {
 		this.memberID = memberID;
 	}
-
-	public void addSubscriber(IMemberUpdateObserver sub) {
-		subscribers.add(sub);
-	}
-
 	public void registerBoat(double length, BoatType type) {
 		this.boatdata.add(new Boat(length, type));
-		for (IMemberUpdateObserver obs : subscribers)
-			obs.memberInformationChanged();
 	}
 		
 	public void updateBoat(Boat boat) {	
 		int index = this.boatdata.indexOf(boat);
 		this.boatdata.get(index).setLength(boat.getLength());
 		this.boatdata.get(index).setType(boat.getType());
-		for (IMemberUpdateObserver obs : subscribers)
-			obs.memberInformationChanged();
 	}
 	
 	public void deleteBoat(Boat boat) {
 		this.boatdata.remove(boat);
-		for (IMemberUpdateObserver obs : subscribers)
-			obs.memberInformationChanged();
 	}
 	
 /***********************************Below methods I am not using*************************************************************/	
-	
-	
-	public void registerBoat(double length, int type) {
-		this.getBoatdata().add(new Boat(length, BoatType.values()[type - 1]));
-		for (IMemberUpdateObserver obs : subscribers)
-			obs.memberInformationChanged();
-	}	
-	
+
 	public void updateBoat(double length, BoatType type, int index) {
 		if (length != -1)
 			this.getBoatdata().get(index - 1).setLength(length);
 		if (type != null)
 			this.getBoatdata().get(index - 1).setType(type);
 
-		for (IMemberUpdateObserver obs : subscribers)
-			obs.memberInformationChanged();
 	}
 
 	public void deleteBoat(int index) {
 		this.getBoatdata().remove(index - 1);
-		for (IMemberUpdateObserver obs : subscribers)
-			obs.memberInformationChanged();
 	}
 }
